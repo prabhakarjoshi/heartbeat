908fe4a7a0099158e6b3e63745d70988f8353a03
Prabhakar Joshi <70520718+prabhakarjoshi@users.noreply.github.com>
2023-02-09 11:50:06 +0530

    Delete pro3 directory

--- pro3/config.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot as plt
-import time
-
-heartbeat_count = 128
-fig = plt.figure()
-ax = fig.add_subplot(111)
-heartbeat_values = [0] * heartbeat_count
-heartbeat_times = [time.time()] * heartbeat_count
